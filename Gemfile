93ab38ac54fb2dd183e92025969bde3a7616d532
Eno Compton 4 <eno4@ecom.com>
2013-12-15 01:13:47 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '4.0.2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JsonRails::Application.config.secret_key_base = '98c57dee58abd58bae2fb48e704754fef46df62f16e5ccb1d3eed622b592e71a1912488472883d3b6b4c01c0a651377c0c95f9db644f1012f3f0c3a513f7a37c'
