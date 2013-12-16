fa4247e0e10bf03103390f0c1ecbdbbfe0d7356d
Eno Compton 4 <eno4@ecom.com>
2013-12-15 16:09:20 -0800

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215190825) do
+ActiveRecord::Schema.define(version: 20131215170206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
