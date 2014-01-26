8dc50c4ee4b5785f795fb0181e99fd98b8c05f43
Eno Compton 4 <eno4@ecom.com>
2014-01-25 22:54:25 -0800

    Remove static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 JsonRails::Application.routes.draw do
   devise_for :users, controllers: { sessions: "sessions" }
   resources :movies, only: [:index, :show, :update, :create, :destroy]
-  root to: "static_pages#index"
 end
