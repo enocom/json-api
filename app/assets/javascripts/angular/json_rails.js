578cf40711c4ae1e60b32e70c5fe03e74de07ff5
Eno Compton 4 <eno4@ecom.com>
2014-01-26 01:00:57 -0800

    Add comment in JS code about CSRF config

--- app/assets/javascripts/angular/json_rails.js
+++ app/assets/javascripts/angular/json_rails.js
@@ -1,5 +1,10 @@
 var app = angular.module("jsonRails", ["rails"]);
 
+//app.config(function($httpProvider) {
+//  var authToken = $("meta[name=\"csrf-token\"]").attr("content");
+//  $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+//});
+
 app.factory("Movie", function(railsResourceFactory) {
   return railsResourceFactory({ url: "/api/movies", name: "movie" });
 });
