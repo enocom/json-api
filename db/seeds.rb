8d7cd2b76858e27e31cfccb7d9746174c161a010
Eno Compton 4 <eno4@ecom.com>
2014-03-30 15:31:03 -0700

    Create basic seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+  { title: "Seven Samurai", director: "Akira Kurozawa" },
+  { title: "Dune", director: "Alan Smithee" },
+  { title: "2001", director: "Stanley Kubrick" }
+].each do |movie_info|
+  Api::Movie.create(title: movie_info[:title],
+                    director: movie_info[:director])
+end
