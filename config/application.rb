cadb1d3710fdd3bcdeb381a05bd6206732b27ff0
Eno Compton 4 <eno4@ecom.com>
2014-05-28 21:01:02 -0700

    Require dependencies where necessary
    This is a result of calling Bundler.setup in place of Bundler.require
    inside applicaiton.rb.

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,12 @@ require "sprockets/railtie"
 
 Bundler.setup(:default, Rails.env)
 
+# Asset pipeline specific gems
+require "jquery-rails"
+require "angularjs-rails-resource"
+require "angularjs-rails"
+require "sass-rails"
+
 module JsonRails
   class Application < Rails::Application
     config.i18n.enforce_available_locales = true
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,4 @@
+require "devise"
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
