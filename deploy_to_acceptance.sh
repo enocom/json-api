280eea64d5d2b0fdadf450cf342b32c480e3d2f1
Eno Compton 4 <eno4@ecom.com>
2014-08-30 14:35:13 -0600

    Specify full heroku remote URL

--- deploy_to_acceptance.sh
+++ deploy_to_acceptance.sh
@@ -3,13 +3,13 @@
 set -x -e
 
 echo "Pushing to heroku acceptance environment"
-git push heroku master
+git push git@heroku.com:json-rails-acceptance.git master
 
 echo "Migrating acceptance database"
-heroku run rake db:migrate
+heroku run rake db:migrate --app json-rails-acceptance
 
 echo "Restarting app"
-heroku restart
+heroku restart --app json-rails-acceptance
 
 echo "Tagging current commit as deployed to acceptance"
 tag_name="acceptance-$(date +%s)"
