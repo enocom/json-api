f86720a61564146f5cdea6b0c112390d58a38f9b
Eno Compton 4 <eno4@ecom.com>
2014-08-30 12:32:44 -0600

    Extract a common render error method in controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -9,37 +9,38 @@ module Api
     def show
       respond_with MovieService.new.find(params[:id])
     rescue MovieService::MovieLookupError => e
-      error_response = { "error" => e.to_s }.to_json
-      render json: error_response, status: 400
+      render_error(e)
     end
 
     def update
       respond_with MovieService.new.update(params[:id], movie_params)
     rescue MovieService::MovieLookupError => e
-      error_response = { "error" => e.to_s }.to_json
-      render json: error_response, status: 400
+      render_error(e)
     end
 
     def create
       created_movie = MovieService.new.create(movie_params)
       respond_with created_movie, location: api_movie_path(created_movie.id)
     rescue MovieService::MovieCreationError => e
-      error_response = { "error" => e.to_s }.to_json
-      render json: error_response, status: 400
+      render_error(e)
     end
 
     def destroy
       MovieService.new.destroy(params[:id])
       head :no_content
     rescue MovieService::MovieLookupError => e
-      error_response = { "error" => e.to_s }.to_json
-      render json: error_response, status: 400
+      render_error(e)
     end
 
     private
 
     def movie_params
       params.require(:movie).permit(:title, :director)
+    end
+
+    def render_error(e)
+      error_response = { "error" => e.to_s }.to_json
+      render json: error_response, status: 400
     end
   end
 end
