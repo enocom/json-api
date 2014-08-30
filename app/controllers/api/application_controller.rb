5c85619800a9c9a0270ca441c0f343ab85185d08
Eno Compton 4 <eno4@ecom.com>
2014-08-29 22:09:37 -0600

    Use null session in application controller

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,7 +1,5 @@
 module Api
   class ApplicationController < ActionController::Base
-    # Prevent CSRF attacks by raising an exception.
-    # For APIs, you may want to use :null_session instead.
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :null_session
   end
 end
