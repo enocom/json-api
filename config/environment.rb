929d7fcefcf549b34ca3751c863a4d4e97486390
Eno Compton 4 <eno4@ecom.com>
2014-08-31 01:12:36 -0600

    Add SendGrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 JsonRails::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
+  :domain => 'json-rails.rocks',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
