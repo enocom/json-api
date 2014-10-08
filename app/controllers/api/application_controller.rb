1e1c2a06c8db4b9095fb85bb069a428af115f1e7
Eno Compton 4 <eno4@ecom.com>
2014-10-07 22:31:09 -0600

    Skip CSRF verification for JSON requests

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,5 +1,6 @@
 module Api
   class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
+    protect_from_forgery with: :null_session,
+      if: Proc.new { |c| c.request.format.json? }
   end
 end
