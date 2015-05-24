59f5a8f09834333254770e839b9a1da97bd2ced2
Eno Compton <eno4@ecom.com>
2015-05-24 12:28:49 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
   { title: "Dune", director: "Alan Smithee" },
   { title: "2001", director: "Stanley Kubrick" }
 ].each do |movie_info|
-  Movie.create(title: movie_info[:title],
-                    director: movie_info[:director])
+  MovieManager::MovieRepo.persist(title:    movie_info[:title],
+                                  director: movie_info[:director])
 end
